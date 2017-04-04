665365f247a992e9bbacb52d60633e83a282d58e
Codiant_Macbook_Pro <codiant_macbook_pro@Codiant-Macbook-Pros-MacBook-Pro.local>
2017-04-04 11:53:01 +0530

    App delegate update

--- DemoAlamofire/ViewController.swift
+++ DemoAlamofire/ViewController.swift
@@ -8,34 +8,31 @@
 
 import UIKit
 import Alamofire
-import SwiftyJSON
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     @IBOutlet weak var tblView: UITableView!
     var arrList = [[String:AnyObject]]()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         callAlamofire()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+//MARK: Calling Webservice
     func callAlamofire(){
-        Alamofire.request("http://api.androidhive.info/contacts/").responseData { (response) in
+        Alamofire.request("http://api.androidhive.info/contacts/").responseJSON { (response) in
             print(response)
-            let swiftyJsonVar = JSON(response.result.value!)
-            print(swiftyJsonVar)
-            let responseData = swiftyJsonVar["contacts"].arrayObject
+            let tempNames = response.result.value as? [String : Any]
+            let responseData = tempNames?["contacts"]
             self.arrList = responseData as! [[String:AnyObject]]
             print(self.arrList.count)
             self.tblView.reloadData()
         }
     }
-
+//MARK: TableView Datasource and Delegate Methods
 func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return self.arrList.count
 }
@@ -43,8 +40,12 @@ func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> 
 func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
     let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
     let dict:NSDictionary = self.arrList[indexPath.row]as NSDictionary
+    let arrPhone:NSDictionary = dict.value(forKey: "phone")as! NSDictionary
+    let strPhone:String = arrPhone.value(forKey: "mobile")as! String
+    print(strPhone)
     cell.textLabel?.text = dict.value(forKey: "name")as? String
     cell.detailTextLabel?.text = dict.value(forKey: "email")as? String
     return cell
-}
+   }
+    
 }
